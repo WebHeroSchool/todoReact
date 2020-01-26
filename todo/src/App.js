0f5daf2530e2c91d7f59631885d08d8019ad67ed
Alexandr Dragun <dragun071@gmail.com>
2020-01-26 16:14:40 +0200

    new file number.js

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-const a = 2;
-const b = 3
+import {count,length} from './number.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Edit <code>src/App.js</code> and save to reload.</p>
+        <p>{count*length}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ todo/src/number.js
@@ -0,0 +1,3 @@
+const count = 2;
+const length = 3;
+export {count,length};
