eb3f277fe6732244f1dbf5d2ff6633b9f07f4a56
Alexandr_Dragun <40923870+Dachs12@users.noreply.github.com>
2020-01-29 21:41:26 +0200

    Revert "structure"

--- src/components/InputItem/InputItem.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const InputItem = () => (<input></input>);
-
-export default InputItem;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import App from './components/App/App';
-
-ReactDOM.render(<App/>, document.getElementById('root'));
-=======
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
->>>>>>> 38012c0697607bd24a6b75bbc5d1919db9bd70b6
-=======
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
->>>>>>> master
