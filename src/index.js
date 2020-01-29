118e86b932bd2db4763f499488852dd4f7dcbef1
Alexandr Dragun <dragun071@gmail.com>
2020-01-29 21:19:33 +0200

    add structure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+<<<<<<< HEAD
 import App from './components/App/App';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
+=======
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+>>>>>>> 38012c0697607bd24a6b75bbc5d1919db9bd70b6
